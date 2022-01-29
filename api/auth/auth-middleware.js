f26dba2b444f2ec0f464d2d0eb2d8aa80223e22d
richad.97 <rcastillo2022@gmail.com>
2022-01-28 19:32:10 -0800

    completed tests

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -18,6 +18,7 @@ const restricted = (req, res, next) => {
     Put the decoded token in the req object, to make life easier for middlewares downstream!
   */
   const token = req.headers.authorization;
+
   if (!token) {
     next({ status: 401, message: "Token required" });
   } else {
@@ -33,7 +34,7 @@ const restricted = (req, res, next) => {
   }
 };
 
-const only = (role_name) => (req, res, next) => {
+const only = (role) => (req, res, next) => {
   /*
     If the user does not provide a token in the Authorization header with a role_name
     inside its payload matching the role_name passed to this function as its argument:
@@ -44,10 +45,10 @@ const only = (role_name) => (req, res, next) => {
 
     Pull the decoded token from the req object, to avoid verifying it again!
   */
-  if (req.decodedJWT.role === role_name) {
+  if (req.decodedJWT.role_name === role) {
     next();
   } else {
-    next({ status: 403, message: "you have no power here!" });
+    next({ status: 403, message: "This is not for you" });
   }
 };
 
@@ -63,9 +64,9 @@ const checkUsernameExists = (req, res, next) => {
 };
 
 const validateRoleName = (req, res, next) => {
-  //  If the role_name in the body is valid, set req.role_name to be the trimmed string and proceed.
-
   /*
+   If the role_name in the body is valid, set req.role_name to be the trimmed string and proceed.
+
     If role_name is missing from req.body, or if after trimming it is just an empty string,
     set req.role_name to be 'student' and allow the request to proceed.
 
@@ -81,7 +82,25 @@ const validateRoleName = (req, res, next) => {
       "message": "Role name can not be longer than 32 chars"
     }
   */
-  next();
+  const role_name = req.body.role_name;
+
+  if (!role_name || role_name === "") {
+    req.body.role_name = "student";
+    next();
+  } else if (role_name) {
+    let trimmedRoleName = role_name.trim();
+
+    if (trimmedRoleName.length > 32) {
+      res
+        .status(422)
+        .json({ message: "Role name can not be longer than 32 chars" });
+    } else if (trimmedRoleName === "admin") {
+      res.status(422).json({ message: "Role name can not be admin" });
+    } else {
+      req.body.role_name = trimmedRoleName;
+      next();
+    }
+  }
 };
 
 module.exports = {
--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -59,6 +59,7 @@ router.post("/login", checkUsernameExists, (req, res, next) => {
     if (user && bcrypt.compareSync(password, user.password)) {
       // here we make token and send it to client in res.body
       const token = tokenBuilder(user);
+
       res.status(200).json({ message: `${user.username} is back!`, token });
     } else {
       next({ status: 401, message: "Invalid Credentials" });
